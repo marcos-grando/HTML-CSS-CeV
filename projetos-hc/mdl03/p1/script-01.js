23a9cb36fc43440a654bf880c32433e52e63d89e
MarcosAGrando <marcosgrando.03@gmail.com>
2024-07-07 00:19:04 -0300

    commit

---
+++ projetos-hc/mdl03/p1/script-01.js
@@ -0,0 +1,29 @@
+const cor1 = "linear-gradient(180deg, aqua, aqua)"
+const cor2 = "linear-gradient(180deg, orange, yellow)"
+const cor3 = "url('imagens/pattern001.png')"
+const cor4 = "url('Imagens/pattern002.png')"
+const cor5 = "url('Imagens/pattern003.png')"
+const cor6 = "url('Imagens/pattern004.png')"
+const cor7 = "url('Imagens/wallpaper006.jpg')"
+
+function cor01(){
+    document.querySelector('div#bf01').style.backgroundImage=`${cor1}`
+}
+function cor02(){
+    document.querySelector('div#bf01').style.backgroundImage=`${cor2}`
+}
+function cor03(){
+    document.querySelector('div#bf01').style.backgroundImage=`${cor3}`
+}
+function cor04(){
+    document.querySelector('div#bf01').style.backgroundImage=`${cor4}`
+}
+function cor05(){
+    document.querySelector('div#bf01').style.backgroundImage=`${cor5}`
+}
+function cor06(){
+    document.querySelector('div#bf01').style.backgroundImage=`${cor6}`
+}
+function cor07(){
+    document.querySelector('div#bf01').style.backgroundImage=`${cor7}`
+}
